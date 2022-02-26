e45aed23da6fef3815725821a490c2951418e476
ZUOQIN HU (Isabella) <eunicejhu@gmail.com>
2022-02-26 17:16:04 +0100

    feat: add function

--- src/threeSum.ts
+++ src/threeSum.ts
@@ -1,10 +1,10 @@
-export function threeSum(nums: number[]): number[][] {
+export function threeSum(nums: number[]): number[][] | false {
   if (nums.length < 3) {
-    return [];
+    return false;
   }
   let result = [];
   for (let i = 0; i < nums.length - 2; i++) {
-    for (let j = i; j < nums.length - 1; j++) {
+    for (let j = 1; j < nums.length - 1; j++) {
       for (let k = 2; k < nums.length; k++) {
         if (nums[i] + nums[j] + nums[k] === 0) {
           result.push([nums[i], nums[j], nums[k]]);
@@ -12,5 +12,6 @@ export function threeSum(nums: number[]): number[][] {
       }
     }
   }
-  return result;
+
+  return result.length > 0 ? result : false;
 }
