acdce1cfe52e36fe677494cf9b14e86698d70c9e
ZUOQIN HU (Isabella) <eunicejhu@gmail.com>
2022-07-16 10:01:31 +0200

    feat: improve lang

--- __tests__/lang.test.ts
+++ __tests__/lang.test.ts
@@ -3,3 +3,7 @@ import { parseSentence } from "@src/lang";
 test("valid input", () => {
   expect(parseSentence(["hello", "world"])).toBe("hello world");
 });
+
+test("valid number input", () => {
+  expect(parseSentence(["hello", 2])).toBe("hello 2");
+});
--- src/lang.ts
+++ src/lang.ts
@@ -1,8 +1,10 @@
-const parseSentence = (words: string[]): string =>
-  words.reduce(
+const parseSentence = (words: (string | number)[]): string =>
+  numberToString(words).reduce(
     (chain, word, index) =>
       (chain += `${word}${index == words.length - 1 ? "" : " "}`),
     ""
   );
 
+const numberToString = (words: (string | number)[]): string[] =>
+  words.map((word) => word.toString());
 export { parseSentence };
